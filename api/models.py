0df963e5cc09965e9714e742dffbd71554be208f
Igor Vertepny <ivertepny@gmail.com>
2025-05-23 22:56:01 +0300

    add model for Video

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class VideoInsight(models.Model):
+    video_id = models.CharField(max_length=32, unique=True)
+    title = models.TextField()
+    description = models.TextField(blank=True)
+    views = models.PositiveIntegerField()
+    subs = models.PositiveIntegerField()
+    score = models.FloatField()
+    insight = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title[:50]} ({self.score})"
